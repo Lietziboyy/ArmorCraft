daa8fbb798890184ee857acbbdbc51cf0749c358
Lietziboy <michael.lietz@gmx.at>
2023-11-05 17:38:10 +0100

    Added Custom Items

--- src/main/java/lietziboy/projectarmorcraft/ProjectArmorCraft.java
+++ src/main/java/lietziboy/projectarmorcraft/ProjectArmorCraft.java
@@ -1,15 +1,15 @@
 package lietziboy.projectarmorcraft;
 
+import lietziboy.projectarmorcraft.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Projectarmorcraft implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("projectarmorcraft");
+public class ProjectArmorCraft implements ModInitializer {
+
+	public static final String MOD_ID = "projectarmorcraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
@@ -18,5 +18,7 @@ public class Projectarmorcraft implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+
 	}
 }
---
+++ src/main/java/lietziboy/projectarmorcraft/item/ModItems.java
@@ -0,0 +1,30 @@
+package lietziboy.projectarmorcraft.item;
+
+import lietziboy.projectarmorcraft.ProjectArmorCraft;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import static lietziboy.projectarmorcraft.ProjectArmorCraft.MOD_ID;
+
+public class ModItems {
+
+    public static final Item RUBBER = registerItem("rubber",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item RAW_RUBBER = registerItem("raw_rubber",
+        new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        ProjectArmorCraft.LOGGER.debug("Registering Items for " + MOD_ID);
+    }
+
+}
