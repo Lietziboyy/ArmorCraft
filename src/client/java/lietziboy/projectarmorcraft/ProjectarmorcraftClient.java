e9e268f3802e16182c3d15e71f125de839f1a539
Lietziboy <michael.lietz@gmx.at>
2023-11-05 15:56:12 +0100

    Inital commit

---
+++ src/client/java/lietziboy/projectarmorcraft/ProjectarmorcraftClient.java
@@ -0,0 +1,10 @@
+package lietziboy.projectarmorcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProjectarmorcraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/lietziboy/projectarmorcraft/ProjectarmorcraftDataGenerator.java
@@ -0,0 +1,11 @@
+package lietziboy.projectarmorcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ProjectarmorcraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
